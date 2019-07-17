1edf2d00609c8d5383fb0e811b8267a6168e4c7b
kitaaaaa <ryo.bawdies@gmail.com>
2019-07-17 15:12:47 +0900

    [finish]2-3

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,4 @@
 #     https: false
 #   )
 # end
+Refile.secret_key = 'f43b11978dbf872bca997470e1c82f6e8fbc5a73fb1a32f2946595003798ca683b07cd160180a73ed76efa4aa6dfe2608650400bbcae54d1a9bc33f4977bb557'
